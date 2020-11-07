7669ddad19ff44f8ef9538b79c4e0cb70b3fb6f3
echiloncruz <echiloncruz@alumno.unlam.edu.ar>
2020-11-07 17:20:21 -0300

    Eduar: cargos con clase enum

---
+++ src/ar/unlam/edu/rrhh/Cargo.java
@@ -0,0 +1,11 @@
+package ar.unlam.edu.rrhh;
+
+public enum Cargo {
+	
+	GerenteGeneral,
+	Produccion, GestionDeCalidad, Fabricacion, Insumos,
+	Ventas, Finanzas, RRPP, Marketing,
+	RRHH, RelacionesLaborales, Seleccion, SalariosYBeneficios,
+	Administracionn, Contabilidad, Tesoreria;
+
+}
