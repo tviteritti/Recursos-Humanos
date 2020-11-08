037a398adda6c7d656a52e424471f9e50166f0fb
Ian Wagner <ianwagnerlautaro@gmail.com>
2020-11-08 00:15:01 -0300

    Tests

--- test/ar/unlam/edu/rrhh/testIan.java
+++ test/ar/unlam/edu/rrhh/testIan.java
@@ -4,11 +4,71 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ar.unlam.edu.persona.*;
+import ar.unlam.edu.calendario.*;
+import ar.unlam.edu.rrhh.*;
+
+
 public class testIan {
 
 	@Test
-	public void test() {
+	public void JefeGerenteyEmpleadoCreadosCorrectamente() {
+		
+		Persona jefe=new Jefe(null, null, null, null, null, null, null, null);
+		Persona gerente=new Gerente(null, null, null, null, null, null, null, null);
+		Persona empleado=new Empleado(null, null, null, null, null, null, null, null);
+		
+		assertNotNull(jefe);
+		assertNotNull(gerente);
+		assertNotNull(empleado);
 		
 	}
+	
+	@Test
+	public void QueSecalculeCorrectamenteElSalarioBruto() {
+		
+		Persona jefe=new Jefe(null, null, null, null, null, 1000.0, 10, null);
+		
+		Double Salario=1000.0;
+		Integer antiguedad=10;
+		Double salarioBruto=Salario+(Salario*5/100)*antiguedad;
+		
+		assertEquals(jefe.salarioBruto(), salarioBruto);
+		
+		
+	}
+	
+	@Test
+	public void QueSecalculeCorrectamenteLasFaltasDelMes() {
+		
+		Persona jefe=new Jefe(null, null, null, null, null, null, null, null);
+		
+		Integer mes=8;
+		Integer faltas=9;
+		Integer ausentismo[]=new Integer[12];
+		ausentismo[mes-1]=faltas;
+		
+		jefe.setAusentismo(ausentismo);
+		
+		assertEquals(jefe.obtenerFaltasDelMes(mes), faltas);
+		
+		
+	}
+	
+	@Test
+	public void QueCalculeCorrectamenteLaAntiguedad() {
+		
+		Integer anosAntiguedad=10;
+		Persona jefe=new Jefe(null, null, null, null, null, null, anosAntiguedad, null);
+		
+		Double antiguedad=(double)anosAntiguedad*(anosAntiguedad*5/100);
+		
+		
+		assertEquals(jefe.calcularAntiguedad(), antiguedad);
+		
+		
+	}
+	
+	
 
 }
